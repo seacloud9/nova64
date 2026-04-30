c7338603a238f8477a4670f60d9830fda1fab537
seacloud9 <brendonsmith@seacloud9.org>
2026-04-29 21:39:11 -0700

    feat(godot): make code.js editable in script editor with JS syntax highlighting

---
+++ nova64-godot/godot_project/addons/nova64_carts/js_syntax_highlighter.gd
@@ -0,0 +1,67 @@
+@tool
+extends EditorSyntaxHighlighter
+
+# Lightweight JavaScript syntax highlighter for the Godot script editor.
+# Registered via the nova64_carts addon so files ending in `.js` get readable
+# colours without needing a full LSP-grade parser.
+
+const KEYWORD_COLOR := Color(1.0, 0.44, 0.52)
+const CONTROL_COLOR := Color(1.0, 0.71, 0.36)
+const TYPE_COLOR    := Color(0.51, 0.85, 1.0)
+const LITERAL_COLOR := Color(0.78, 0.55, 1.0)
+const STRING_COLOR  := Color(1.0, 0.93, 0.55)
+const COMMENT_COLOR := Color(0.5, 0.55, 0.6)
+const NUMBER_COLOR  := Color(0.66, 0.92, 0.66)
+const SYMBOL_COLOR  := Color(0.8, 0.85, 0.95)
+const FUNCTION_COLOR := Color(0.55, 0.78, 1.0)
+const MEMBER_COLOR  := Color(0.85, 0.85, 1.0)
+
+const KEYWORDS := [
+	"class", "const", "debugger", "delete", "export", "extends", "function",
+	"import", "in", "instanceof", "let", "new", "of", "super", "this",
+	"typeof", "var", "void", "with", "yield", "async", "await", "from",
+	"as", "static", "get", "set",
+]
+const CONTROL_KEYWORDS := [
+	"break", "case", "catch", "continue", "default", "do", "else",
+	"finally", "for", "if", "return", "switch", "throw", "try", "while",
+]
+const LITERALS := ["true", "false", "null", "undefined", "NaN", "Infinity"]
+const TYPES := [
+	"Array", "Object", "String", "Number", "Boolean", "Math", "JSON",
+	"Promise", "Map", "Set", "Symbol", "Error", "Date", "RegExp",
+	"Float32Array", "Uint8Array", "Int32Array", "console",
+]
+
+var _ch: CodeHighlighter = CodeHighlighter.new()
+
+func _init() -> void:
+	_ch.number_color = NUMBER_COLOR
+	_ch.symbol_color = SYMBOL_COLOR
+	_ch.function_color = FUNCTION_COLOR
+	_ch.member_variable_color = MEMBER_COLOR
+	for k in KEYWORDS:
+		_ch.add_keyword_color(k, KEYWORD_COLOR)
+	for k in CONTROL_KEYWORDS:
+		_ch.add_keyword_color(k, CONTROL_COLOR)
+	for k in LITERALS:
+		_ch.add_keyword_color(k, LITERAL_COLOR)
+	for k in TYPES:
+		_ch.add_keyword_color(k, TYPE_COLOR)
+	_ch.add_color_region("//", "", COMMENT_COLOR, true)
+	_ch.add_color_region("/*", "*/", COMMENT_COLOR, false)
+	_ch.add_color_region("\"", "\"", STRING_COLOR, false)
+	_ch.add_color_region("'", "'", STRING_COLOR, false)
+	_ch.add_color_region("`", "`", STRING_COLOR, false)
+
+func _get_name() -> String:
+	return "JavaScript"
+
+func _get_supported_languages() -> PackedStringArray:
+	return PackedStringArray(["js"])
+
+func _get_line_syntax_highlighting(p_line: int) -> Dictionary:
+	var te := get_text_edit()
+	if te != null and _ch.get_text_edit() != te:
+		_ch.set_text_edit(te)
+	return _ch._get_line_syntax_highlighting(p_line)
--- nova64-godot/godot_project/addons/nova64_carts/plugin.gd
+++ nova64-godot/godot_project/addons/nova64_carts/plugin.gd
@@ -2,16 +2,16 @@
 extends EditorPlugin
 
 const CART_IMPORTER := preload("res://addons/nova64_carts/cart_importer.gd")
-const CART_SCRIPT_IMPORTER := preload("res://addons/nova64_carts/cart_script_importer.gd")
+const JS_HIGHLIGHTER := preload("res://addons/nova64_carts/js_syntax_highlighter.gd")
 
 var _importer: EditorImportPlugin
-var _script_importer: EditorImportPlugin
+var _highlighter: EditorSyntaxHighlighter
 
 func _enter_tree() -> void:
 	_importer = CART_IMPORTER.new()
 	add_import_plugin(_importer)
-	_script_importer = CART_SCRIPT_IMPORTER.new()
-	add_import_plugin(_script_importer)
+	_highlighter = JS_HIGHLIGHTER.new()
+	add_syntax_highlighter(_highlighter)
 	var fs := EditorInterface.get_resource_filesystem()
 	if fs:
 		fs.scan()
@@ -20,6 +20,6 @@ func _exit_tree() -> void:
 	if _importer:
 		remove_import_plugin(_importer)
 		_importer = null
-	if _script_importer:
-		remove_import_plugin(_script_importer)
-		_script_importer = null
+	if _highlighter:
+		remove_syntax_highlighter(_highlighter)
+		_highlighter = null
